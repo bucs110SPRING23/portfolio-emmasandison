29231dce11b7f10098b605c1d23d94988942efa3
emmasandison <esandis1@binghamton.edu>
2023-02-01 16:52:21 -0500

    update

--- ch01/exercises/colors.py
+++ ch01/exercises/colors.py
@@ -1,4 +1,10 @@
-mycolors=["Pink", "Orange", "Blue", "Green", "Purple"]
+mycolors=[
+    "Pink",
+    "Orange", 
+    "Blue", 
+    "Green", 
+    "Purple"
+]
 list_size=len(mycolors)
 print("I have",list_size,"colors")
 print(mycolors[0])
