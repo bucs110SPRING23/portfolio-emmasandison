8652182a568c7a37d2bc5bcd8a5f60c7f8dfc9fc
emmasandison <esandis1@binghamton.edu>
2023-02-01 20:57:14 -0500

    ch01 lab

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,16 @@
+# Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+classes_per_week = 5
+cost_per_class = (cost_per_week / classes_per_week)
+print("Each class costs $",cost_per_class,"per week")
+print(weeks, type(weeks))
+print(classes, type(classes))
+print(tuition, type(tuition))
+print(cost_per_week,type(cost_per_week))
+print(classes_per_week, type(classes_per_week))
+print(cost_per_class, type(cost_per_class))
 
