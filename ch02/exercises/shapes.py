ed666fc363735cd886045d51a72274ef1405e911
emmasandison <esandis1@binghamton.edu>
2023-02-15 17:56:24 -0500

    update

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -6,7 +6,7 @@ while 1:
     screen = pygame.display.set_mode()
     dimensions = screen.get_size()
     starting_point = (dimensions[0] // 2, dimensions[1] // 2)
-    radius = 50
+    radius = 200
     for _ in range(4):
         pygame.draw.circle(screen, "red", starting_point, radius)
         starting_point[1] = starting_point[1] - radius
