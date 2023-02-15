5361ee6750e92a09d33dfd6c93025ae55c63be64
emmasandison <esandis1@binghamton.edu>
2023-02-15 17:27:09 -0500

    new

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -1,9 +1,39 @@
 import pygame
-
 pygame.init()
 
-screen = pygame.display.set_mode()
+while 1: 
+    pygame.event.pump()
+    screen = pygame.display.set_mode()
+    dimensions = screen.get_size()
+    starting_point = (dimensions[0] // 2, dimensions[1] // 2)
+    radius = 50
+    for _ in range(4):
+        pygame.draw.circle(screen, "red", starting_point, radius)
+        starting_point[1] = starting_point[1] - radius
+        radius = radius // 2
+        starting_point[1] = starting_point[1] - radius
+    pygame.display.flip()  
+    pygame.time.wait(2000)
+    break
 
-pygame.draw.circle(screen, "red", [200,  150], 50)
-pygame.draw.circle(screen, "blue", [300, 250], 100)
-pygame.draw.circle(screen, "green", [400, 350], 150)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
