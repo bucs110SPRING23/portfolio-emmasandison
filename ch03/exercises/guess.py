48792b73da90dd910897804a21796fba6f1d2a74
emmasandison <esandis1@binghamton.edu>
2023-02-20 21:33:30 -0500

    new

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,17 +1,18 @@
 import random 
 
-randnum = random.randint(1,11)
-guess = int(input("Guess a number 1-10: "))
-attempts = 3
+randnum = random.randrange(1,11)
+attempts = 0
 
-for attempt in range(attempts): 
-  if randnum == guess: 
-    print("Correct!")
-    break
-  else:
+for _ in range(3): 
+    guess = int(input("Guess a number 1-10: "))
+    attempts = attempts + 1
     if guess < randnum:
-       print("Too low")
-    if guess > randnum: 
-       print("Too high")
+        print("Too low")
+    elif guess > randnum:
+        print("Too high")
+    elif randnum == guess: 
+        print("Correct!")
+        break
 
-    
+print("It took", attempts, "guesses")
+
