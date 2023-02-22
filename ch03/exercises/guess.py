4c552e2a6ad3609c352d2aa5ae0f33d9671d66fc
emmasandison <esandis1@binghamton.edu>
2023-02-22 17:38:57 -0500

    update

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -3,7 +3,7 @@ import random
 randnum = random.randrange(1,11)
 attempts = 0
 
-for _ in range(3): 
+for _ in range(3): # [0,1,2]- so 3 guesses
     guess = int(input("Guess a number 1-10: "))
     attempts = attempts + 1
     if guess < randnum:
