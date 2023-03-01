8c228f13e1b0db5e000a4912673bd4104853ff2d
emmasandison <esandis1@binghamton.edu>
2023-02-28 22:12:20 -0500

    update

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -3,56 +3,35 @@ import math
 import random
 pygame.init()
 
+ 
 width = 500
 height = 500
 screen = pygame.display.set_mode([width,height])
 surface1 = pygame.Surface([width,height])
+surface2 = pygame.Surface([width,height])
 
-x = 250
-y = 250
-center = (x,y)
-surface1.fill("blue")
-pygame.draw.circle(surface1, "orange", center, 250)
-x2 = 250
-y2 = 0
-start_pos = (x2,y2)
-x3 = 250
-y3 = 500
-end_pos = (x3,y3)
-pygame.draw.line(surface1, "purple", start_pos, end_pos, 4)
-x4 = 0
-y4 = 250
-start_pos2 = (x4,y4)
-x5 = 500
-y5 = 250
-end_pos2 = (x5,y5)
-pygame.draw.line(surface1, "purple", start_pos2, end_pos2, 4)
-pygame.display.flip()
-
-
-for i in range(10): 
-    x1 = random.randrange(0,width)
-    y1 = random.randrange(0,height)
-    coordinates = (x1, y1)
-    distance_from_center = math.hypot(x1-x, y1-y)
-    is_in_circle = distance_from_center <= width/2
-    if is_in_circle == False:
-        pygame.draw.circle(surface1,"red",coordinates,3)
-        pygame.display.flip()
-        pygame.time.wait(2500)
-    if is_in_circle == True: 
-        pygame.draw.circle(surface1,"green",coordinates,3)
-        pygame.display.flip()
-        pygame.time.wait(2000)
- 
-
-surface1.fill("white")
 choice = ""
 mouse_position = pygame.mouse.get_pos()
-blue = pygame.draw.rect(surface1, "blue", (50,60,100,50))
-red = pygame.draw.rect(surface1, "red", (180,60,100,50))
-pygame.display.flip()
+blue = pygame.draw.rect(surface2, "blue", (0, 0, 250, 500))
+red = pygame.draw.rect(surface2, "red", (250, 0, 250, 500))
 pygame.time.wait(2500)
+screen.blit(surface2, [0,0])
+pygame.display.flip()
+
+
+Guess = True
+while Guess == True: 
+    for event in pygame.event.get(): 
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            if event.pos[0] < width / 2: 
+                playerchoice = "blue"
+                Guess = False 
+            elif event.pos[0] > width / 2: 
+                playerchoice = "red"
+                Guess = False
+print("Your player is", playerchoice)
+pygame.display.flip()
+pygame.time.wait(1000)
 
 print("Who will win, red or blue?")
 pygame.display.flip()
@@ -61,12 +40,6 @@ pygame.time.wait(6000)
 surface1.fill("white")
 pygame.display.flip()
 pygame.time.wait(2500)
-
-width = 500
-height = 500
-screen = pygame.display.set_mode([width,height])
-surface1 = pygame.Surface([width,height])
-
 
 x = 250
 y = 250
@@ -105,9 +78,9 @@ for i in range(10):
             pygame.draw.circle(surface1, player, coordinates, 3)
             pygame.draw.circle(surface1, player, coordinates, 3)
         if player is players[0]:
-            red_score += 1
+            red_score = red_score + 1
         if player is players[1]:
-            blue_score += 1
+            blue_score = blue_score + 1
         pygame.display.flip()
         pygame.time.wait(2500)
         if is_in_circle == False: 
@@ -129,19 +102,6 @@ pygame.display.flip()
 pygame.time.wait(2500)
 
 
-Guess = True
-while Guess == True: 
-    for event in pygame.event.get(): 
-        if event.type == pygame.MOUSEBUTTONDOWN: 
-            if event.pos[0] < x / 2: 
-                playerchoice = "red"
-                Guess = False 
-            elif event.pos[0] > x / 2: 
-                playerchoice = "blue"
-                Guess = False
-print("Your player is", playerchoice)
-pygame.display.flip()
-pygame.time.wait(1000)
 
 if Guess is playerchoice: 
     print("Your guess is correct!")
