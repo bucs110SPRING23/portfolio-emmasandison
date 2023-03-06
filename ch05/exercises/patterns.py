f4844633a97ac2597b7e7acf2042dbe3616181cf
emmasandison <esandis1@binghamton.edu>
2023-03-06 18:05:12 -0500

    new

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,14 @@
+rows = int(input("How many rows?: "))
+
+def star_pyramid(): 
+    for _ in range(1, rows + 1): 
+        number_of_stars = rows - 1
+        print("*")
+
+star_pyramid()
+
+
+
+
+    
+
