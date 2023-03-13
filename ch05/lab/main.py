467460629f2878fc7ff01c9ef34de1800fae508e
emmasandison <esandis1@binghamton.edu>
2023-03-12 22:22:15 -0400

    update

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,44 +1,53 @@
 import pygame 
 
 # Part A
-n = 101
-count = 0
-def threenp1(n): 
-    while n > 1.0: 
-        count += 1
-        if n % 2 == 0: 
-            n = int(n / 2)
-        else: 
-            n = int(3 * n + 1)
-    return count 
-
-def threenp1range(upper_limit):
-
-    list = {}
-    upper_limit = 20
-    iters = []
-    for _ in range(2, upper_limit + 1): 
+def main():
+    n = 101
+    def threenp1(n): 
         count = 0
         while n > 1.0: 
             count += 1
             if n % 2 == 0: 
                 n = int(n / 2)
+                print(n)
             else: 
                 n = int(3 * n + 1)
+                print(n)
+        return count 
+    
+    
+
+    def threenp1range(upper_limit):
+
+        # dict = []
+        upper_limit = 20
+        # iter = {}
+        for _ in range(2, upper_limit + 1): 
+            count = 0
+            while n > 1.0: 
+                count += 1
+                if n % 2 == 0: 
+                    n = int(n / 2)
+                else: 
+                    n = int(3 * n + 1)
+
+                iter = {n:count}
+            return dict
         
-            list[n] = count
-            list.append(n)
-    print(list)
+    # threenp1range()
    
-# Part B 
-def main(): 
+    # Part B 
+ 
+    count = 0
     pygame.init()
     window = pygame.display.set_mode()
     window.fill("white")
     pygame.display.flip()
 
-    def graph_coordinates(list): 
-        coordinates = [(n, count)]
+    def graph_coordinates(threenp1range): 
+        coordinates = [(1, iter[0]), (2, iter[1]), (3, iter[2]), (4, iter[3]), (5, iter[4]), (6, iter[5]), (7, iter[6]), (8, iter[7]), (9, iter[8]), (10, iter[9]), (11, iter[10]), (12, iter[11]), (13, iter[12]), (14, iter[13]), (15, iter[14]), (16, iter[15]), (17, iter[16]), (18, iter[17]), (19, iter[18]), (20, iter[19]), (21, iter[20])]
+        for point in coordinates: 
+            pygame.draw.lines(window, "blue", False, coordinates)
         new_display = pygame.transform.flip(window, False, True)
         width, height = new_display.get_size()
         new_display = pygame.transform.scale(new_display, (100 * 5, 100 * 5))
@@ -50,8 +59,40 @@ def main():
             else: 
                 max_so_far = 0
 
+
+
+    # graph_coordinates()
+
+
+
     font = pygame.font.Font(None, 16)
     msg = font.render("The max so far is: ", True, "green")
     window.blit(msg, (10,10))
+
+    threenp1()
+    threenp1range()
+    graph_coordinates()
+    main()
+
+    window.blit(window, [0,0])
+    exit = False
+    while not exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                exit = True
+    pygame.display.update()
+
+
+    
+
+
+
+
+
+
+
+
+
+
 
             
