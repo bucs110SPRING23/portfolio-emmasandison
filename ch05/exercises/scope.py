14cbf14a10f6008fdadce79c4dd09931e2b38e37
emmasandison <esandis1@binghamton.edu>
2023-03-13 20:00:48 -0400

    new

--- ch05/exercises/scope.py
+++ ch05/exercises/scope.py
@@ -15,6 +15,9 @@ print("The result of", num, "mulitplied by 3 is", result)
 
 # Write a function that takes a number and exponent as parameters and raises the number to the exponent and return the resulting value (no exponentiation)
 def squared(x):
+    """
+    This function takes the number and squares it
+    """
     running_total = 0         
     for counter in range(x):
         running_total = running_total + x
@@ -27,6 +30,9 @@ print("The result of", num, "squared is", result)
 
 # Write another function, called square, that takes a single parameter and squares it by only calling your multiplication or exponent function and return the resulting value only one line of code
 def square(): 
+    """
+    This function uses the previous function to square a number 
+    """
     num = 10
     print("The result of", num, "squared is", squared(10))
 square()
