a42848f2a3895b1aba592a7734f9dc1d87abc664
emmasandison <esandis1@binghamton.edu>
2023-03-20 22:28:02 -0400

    update

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -46,6 +46,10 @@ def main():
     pygame.display.flip()
 
     def graph_coordinates(a): 
+        """
+        This function is supposed to graph the coordinates that are in the dictionary
+        args: a (list)
+        """
         coordinates = [(2, 1), (3, 7), (4, 2), (5, 5), (6, 8), (7, 16), (8, 3), (9, 19), (10, 6), (11, 14), (12, 9), (13, 9), (14, 17), (15, 17), (16, 4), (17, 12), (18, 20), (19, 20), (20, 7)]
         for point in coordinates: 
             pygame.draw.lines(window, "blue", False, coordinates)
