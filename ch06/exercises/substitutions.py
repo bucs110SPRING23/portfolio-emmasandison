7a09a17f1af20c20b29d695096fef24fad90b235
emmasandison <esandis1@binghamton.edu>
2023-03-20 22:10:57 -0400

    new

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,7 +1,35 @@
 import json
 
-fptr = open("subs.json", "r").read()
-fptr2 = open("news.txt", "r").read()
+file1 = open("subs.json", "r")
+file2 = open("news.txt", "r")
 
-for k, v in .items():
+data = {
+    "trump": "master of evil",
+    "arrested": "taken to the dungeon", 
+    "protest": "cat",
+    "pence": "wicked witch of the west", 
+    "law": "demon", 
+    "new york": "wonder world", 
+    "manhattan": "mount olympus"
+}
+
+json_string = json.dumps(data)
+print(json_string, type(json_string))
+json_data = json.loads(json_string)
+
+for k, v in json_data.items(): 
     print(k, v)
+
+file3 = open("betternews.txt", "w")
+file3.write(json_string)
+
+file1.close()
+file2.close()
+
+print(file3.read())
+
+file3.close()
+
+print("\n")
+
+
