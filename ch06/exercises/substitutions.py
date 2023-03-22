fac53dc4b4a96116b730854c6dd6ae5dbc983cb2
emmasandison <esandis1@binghamton.edu>
2023-03-22 17:11:35 -0400

    update

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,16 +1,16 @@
 import json
 
 def main(): 
-    text_fptr = open("news.txt", "r").read().lower() # read entire article in
+    text = open("news.txt", "r").read().lower() # read entire article in
     sub_fptr = open("subs.json", "r")
     subs = json.load(sub_fptr)
     print(subs, type(subs))
 
     for k, v in subs.items(): 
-        text_fptr = text_fptr.replace(k, v)
+        text = text.replace(k, v)
     
     fptr = open("betternews.txt", "w")
-    fptr.write(text_fptr)
+    fptr.write(text)
     fptr.close()
 
 main()
