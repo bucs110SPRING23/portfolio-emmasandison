fad88071fe455615ddd50a008d702d0d7d556e0e
emmasandison <esandis1@binghamton.edu>
2023-03-29 16:57:53 -0400

    update

--- ch07/lab/rectangle.py
+++ ch07/lab/rectangle.py
@@ -5,11 +5,12 @@ class Rectangle:
       self.height = abs(height)
       self.width = abs(width)
 
-    def __str__(self): 
-        """
-        This function returns the paramters of the __init__ in a string
-        """
-        return F"x:{self.x} y:{self.y} width:{self.width} height:{self.height}"
+   def __str__(self):
+      """This function returns the parameters of the __init__ in a string 
+      """
+      return F"x:{self.x} y:{self.y} width{self.width} height:{self.height}" 
 
 # Rectangle (500, 500, 25, 50)
+
+
 
