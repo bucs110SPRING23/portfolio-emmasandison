991b2fa261f591aa01ef15fb0d167a85d17ebd81
emmasandison <esandis1@binghamton.edu>
2023-04-18 15:46:24 -0400

    new

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -10,21 +10,18 @@ class StringUtility:
         return self.string 
     
     def vowels(self):
-        count = 0
-        vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
-        for char in self.string: 
-            if char in vowels:
-                count += 1
-        if count >= 5: 
+        vowels = sum([1 for char in self.string if char.lower() in 'aeiou'])
+        if vowels < 5: 
+            return str(vowels)
+        else: 
             return "many"
-        return str(count)
     
     def bothEnds(self): 
         if len(self.string) <= 2: 
             empty = ""
             return empty
         else: 
-            return self.string[0:1] + self.string[-2:-1]
+            return self.string[:2] + self.string[-2:]
         
     def fixStart(self): 
         if len(self.string) <= 1:
@@ -37,16 +34,29 @@ class StringUtility:
             return str 
     
     def asciiSum(self): 
-        
-        
-
-        
+        str = self.string 
+        integer = sum(ord(char) for char in str)
+        return integer 
     
-        
-        
-
-        
-
-        
+    def cipher(self):
+        result = ""
+        for char in self.string:
+            if char.isalpha():
+            
+                if char.isupper():
+                    start = ord('A')
+                    wrap = 26
+                else:
+                    start = ord('a')
+                    wrap = 26
+                
+                char_ord = ord(char)
+                new_pos = ((char_ord - start + len(self.string)) % wrap) + start
+            
+                result += chr(new_pos)
+            else:
+                result += char
     
+        return result
 
+            
