2af9028ccd6a1ad5bf3c90f2edf4abfd8eb8fd9b
emmasandison <esandis1@binghamton.edu>
2023-04-19 17:48:20 -0400

    commit

--- ch08/exercises/methods.py
+++ ch08/exercises/methods.py
@@ -5,6 +5,7 @@
 # class data: instance variables 
 
 import turtle 
+import pygame
 
 class Num: 
     def __init__(self, n): 
@@ -110,3 +111,13 @@ main()
 # Controller - Logic (director)
 
 # in a gui program, everything is in a class- nothing is in global space 
+
+
+class Snowman(pygame.sprite.Sprite): 
+    def __init__(self, x, y, img="assets/snowman.png"):
+        super().__init__()
+
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
