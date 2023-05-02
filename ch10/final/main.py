776ee997be49a02b3bab748dcd16096fc2ba7ae7
emmasandison <esandis1@binghamton.edu>
2023-05-02 16:46:10 -0400

    commit

--- ch10/final/main.py
+++ ch10/final/main.py
@@ -1,24 +1,27 @@
 from country_api import CountryAPI
 from timezone_api import TimezoneAPI
+from geocoding_api import GeocodingAPI
 
 
 def main(): 
-    country_name = input("Enter a country name: ").lower().strip()
+    location = input("Enter a location (city or country): ").lower().strip()
 
-    country = CountryAPI(country_name)
-    if not country.data: 
-        print(f"No data found for {country_name}")
-        return
-    
-    city = input("Enter a city of the country: ").lower().strip()
+    geocoding = GeocodingAPI(city)
+    lat, lng = geocoding.get_lat_lng()
 
-    timezone = TimezoneAPI(city)
-    formatted_time = timezone.get_local_time()
+    if not lat or not lng: 
+        print(f"No location found")
+
+    country = CountryAPI(lat, lng)
+    city = geocoding.get_city_name()
     
-    if formatted_time:
-        print(f"The current date and time in {city.title()}, {country_name.title()} is {formatted_time}")
+    timezone = TimezoneAPI(lat, lng)
+    time = timezone.get()
+    
+    if time:
+        print(f"The current date and time in {city.title()}, {country.title()} is {time}")
     else: 
-        print(f"No data found for {city.title()}, {country_name.title()}")
+        print(f"No data found for {city.title()}, {country.title()}")
  
 
 main()
