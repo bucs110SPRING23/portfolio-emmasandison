e0296f8c15d4a6a8badd014466cd68cfa8826592
emmasandison <esandis1@binghamton.edu>
2023-05-02 17:15:33 -0400

    commit

--- ch10/final/location_api.py
+++ ch10/final/location_api.py
@@ -1,14 +1,16 @@
 import requests 
 
 class LocationAPI: 
-    def __init__(self, name): 
-        self.name = name
-        self.url = f"https://restcountries.com/v3.1/name/{name}?fullText=true"
+    def __init__(self, city): 
+        self.city = city
+        self.url = f"https://api.opencagedata.com/geocode/v1/json?q={self.city}&key=47255ad0d38641cf960e07579964e7f9"
         self.data = self.get()
 
     def get(self):
         response = requests.get(self.url)
         if response.status_code == 200: 
-            data = response.json()[0]
-            return data
+            data = response.json()
+            if data["total_results"] > 0: 
+                return data["results"][0]["geometry"]
+        return None
         
