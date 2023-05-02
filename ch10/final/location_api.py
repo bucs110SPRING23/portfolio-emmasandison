26f1e0fd4f91b463e2de8e2bc5308a1acff909a5
emmasandison <esandis1@binghamton.edu>
2023-05-02 19:48:59 -0400

    commit

--- ch10/final/location_api.py
+++ ch10/final/location_api.py
@@ -2,9 +2,16 @@ import requests
 
 class LocationAPI:
     def __init__(self):
+        """
+        This function defines the __init__ method nad initializes the url as an instance of LocationAPI
+        """
         self.url = "https://restcountries.com/v3.1/name/{country}?fullText=true"
 
     def get_capital_city(self, country_name):
+        """
+        This function is sending a GET request to the REST countries API to find the capital city of the given country
+        returns capital city 
+        """
         response = requests.get(self.url.format(country=country_name))
         if response.status_code == 200:
             data = response.json()
