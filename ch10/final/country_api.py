ccfe0ca99954d16218c26e71f399766207c52a05
emmasandison <esandis1@binghamton.edu>
2023-05-02 16:54:37 -0400

    commit

--- ch10/final/country_api.py
+++ ch10/final/country_api.py
@@ -15,9 +15,3 @@ class CountryAPI:
             country = data["country"]
             return country
         
-    def titlecase(self): 
-        return self.data.title() if self.data else ""
-        
-    
-       
-    
