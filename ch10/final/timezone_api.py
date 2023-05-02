4394e811e2de7f380b0a35826ca157c404199193
emmasandison <esandis1@binghamton.edu>
2023-05-02 16:51:57 -0400

    commit

--- ch10/final/timezone_api.py
+++ ch10/final/timezone_api.py
@@ -2,9 +2,10 @@ import requests
 from datetime import datetime, timedelta
 
 class TimezoneAPI: 
-    def __init__(self, city): 
-        self.city = city
-        self.url = f"http://api.timezonedb.com/v2.1/get-time-zone?key=LN6DJUMQBB0Z&format=json&by=position&lat=0&lng=0&city={self.city}"
+    def __init__(self, lat, lng): 
+        self.lat = lat
+        self.lng = lng
+        self.url = f"https://api.timezonedb.com/v2.1/get-time-zone?key=LN6DJUMQBB0Z&format=json&by=position&lat={self.lat}&lng={self.lng}"
 
     def get(self): 
         response = requests.get(self.url)
@@ -14,11 +15,6 @@ class TimezoneAPI:
             time = datetime.fromisoformat(time_str)
             offset = timedelta(seconds=data["gmtOffset"])
             local_time = time + offset
-            return local_time
+            return local_time.strftime("%I:%M:%S %p")
         
-    def get_local_time(self): 
-        local_time = self.get()
-        if local_time: 
-            formatted_time = local_time.strftime("%I:%M:%S %p")
-            return formatted_time
        
